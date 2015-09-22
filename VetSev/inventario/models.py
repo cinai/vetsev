1f42f976a11e8c43b5a0d2f3ec5f0705babd0b93
cinai <catta.ugs@gmail.com>
2015-09-22 20:06:18 -0300

    Reordenar modelos, cambiar tablas tipos por choices y definir los campos no obligatorios

--- VetSev/inventario/models.py
+++ VetSev/inventario/models.py
@@ -1,20 +1,50 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 
-# Create your models here.
-class Ingreso(models.Model):
-    descripcion = models.CharField(max_length=200)
-    cantidad = models.CharField(max_length=200)
-    fecha = models.DateTimeField()
-    n_boleta = models.IntegerField(default=0)
-    tipo_pago =models.ForeignKey(Tipo_Pago)
-    n_cheque = models.IntegerField(default=0)
-    caja = models.ForeignKey(Caja)
-    cliente = models.ForeignKey(Cliente)
 
-#Tipo_Pago es solo chars, la idea es hacer un formulario para cada uno
-#bassado en un modelo
-class Tipo_Pago(models.Model):
-    tipo = models.CharField(max_length=200)
+class Item_Productos(models.Model):
+    TIPO_ITEM_CHOICES = (
+        ('Farmacos', 'Fármacos'),
+        ('Alimentos', 'Alimentos'),
+        ('Productos', 'Productos Mascotería'),
+        ('Insumos', 'Insumos'),
+        ('Otros','Otros')
+    )
+    tipo = models.CharField(max_length=20,choices=TIPO_ITEM_CHOICES)
+    descripcion = models.CharField(max_length=100)
+    valor_venta = models.IntegerField(default=0)
+
+
+class Inventario_Producto(models.Model):
+    item = models.ForeignKey(Item_Productos)
+    cantidad = models.IntegerField(default=1)
+    ultima_mod = models.DateTimeField()
+
+
+class Item_Inmueble(models.Model):
+    descripcion = models.CharField(max_length=200)
+    costo = models.IntegerField(default=0)
+
+
+class Inventario_Inmueble(models.Model):
+    item = models.ForeignKey(Item_Inmueble)
+    cantidad = models.IntegerField(default=1)
+    ultima_mod = models.DateTimeField()
+
+
+class Caja(models.Model):
+    TIPO_TURNO_CHOICES = (
+        ('Mañana', 'Mañana'),
+        ('Tarde', 'Tarde'),
+    )
+    monto = models.IntegerField(default=0)
+    saldo = models.IntegerField(default=0)
+    entregaDoc = models.IntegerField(default=0,blank=True)
+    tipo_turno = models.CharField(max_length=6,choices=TIPO_TURNO_CHOICES)
+    fechaCreacion = models.DateTimeField()
+    fechaCierre = models.DateTimeField()
+    revisado = models.BooleanField(blank=True,default=False)
+
 
 class Cliente(models.Model):
     nombre = models.CharField(max_length=40)
@@ -22,40 +52,48 @@ class Cliente(models.Model):
     telefono = models.CharField(max_length=14)
     direccion = models.CharField(max_length=100)
 
-class Caja(models.Model):
-    monto = models.IntegerField(default=0)
-    saldo = models.IntegerField(default=0)
-    entregaDoc = models.IntegerField(default=0)
-    tipo_turno = models.ForeignKey(Tipo_Turno)
-    fechaCreacion = models.DateTimeField()
-    fechaCierre = models.DateTimeField()
 
-class Tipo_Turno(models.Model):
-    tipo = models.CharField(max_length=30,required=True,primary_key=True)
+# Tipo_Pago es solo chars, la idea es hacer un formulario para cada uno
+# bassado en un modelo
+class Ingreso(models.Model):
+    TIPO_PAGO_CHOICES =(
+        ('Efectivo', 'Efectivo'),
+        ('RedCompra', 'RedCompra'),
+        ('Cheque', 'Cheque'),
+    )
+    descripcion = models.CharField(max_length=200)
+    cantidad = models.CharField(max_length=200)
+    fecha = models.DateTimeField()
+    n_boleta = models.IntegerField(default=0)
+    tipo_pago =models.CharField(max_length=15,choices=TIPO_PAGO_CHOICES)
+    n_cheque = models.IntegerField(default=0,blank=True)
+    caja = models.ForeignKey(Caja)
+    cliente = models.ForeignKey(Cliente)
+
 
 class Observacion(models.Model):
     Descripcion = models.CharField(max_length=300)
     fecha = models.DateTimeField()
     caja = models.ForeignKey(Caja)
 
-class Venta(models.Model):
-    cantidad = models.IntegerField(default=1)
-    item = models.ForeignKey(Item)
-    ingreso = models.ForeignKey(Ingreso)
+
+class Labor(models.Model):
+    nombre = models.CharField(max_length=10)
+
 
 class Trabajador(models.Model):
     nombre = models.CharField(max_length=50)
+    labor = models.ForeignKey(Labor)
     rut = models.CharField(max_length=12,primary_key=True)
     telefono = models.CharField(max_length=12)
 
-class Cargo(models.Model):
-    nombre = models.CharField(max_length=10, default="Veterinario")
 
 class Horario(models.Model):
-    dia = models.CharField()
+    dia = models.CharField(max_length=10)
     hora_inicio = models.IntegerField()
     hora_salida = models.IntegerField()
     trabajador = models.ForeignKey(Trabajador)
+
 
 class Pago_Sueldos(models.Model):
     descripcion = models.CharField(max_length=100)
@@ -63,51 +101,46 @@ class Pago_Sueldos(models.Model):
     monto = models.IntegerField(default=0)
     fecha = models.DateTimeField()
 
+
+class Venta(models.Model):
+    cantidad = models.IntegerField(default=1)
+    item = models.ForeignKey(Item_Productos)
+    ingreso = models.ForeignKey(Ingreso)
+
+
 class Compra_A_Proveedor(models.Model):
-    item = models.ForeignKey(Item_Ventas)
+    item = models.ForeignKey(Item_Productos)
     cantidad = models.IntegerField(default=1)
     costo = models.IntegerField(default=0)
     fecha = models.DateTimeField()
 
+
 class Egreso_Productos(models.Model):
-    item = models.ForeignKey(Item_Ventas)
+    item = models.ForeignKey(Item_Productos)
     motivo = models.CharField(max_length=100)
     fecha = models.DateTimeField()
 
-class Pago_Cuentas(models.Model):
-    tipo = models.ForeignKey(Tipo_Cuenta)
-    descripci�n = models.CharField(max_length=100)
-    monto = models.IntegerField(default=0)
-    fecha = models.DateTimeField()
 
-class Tipo_Cuenta(models.Model):
-    tipo = models.CharField(max_length=40)
+class Pago_Cuentas(models.Model):
+    TIPO_CUENTA_CHOICES= (
+        ('Agua', 'Agua'),
+        ('Arriendo', 'Arriendo'),
+        ('Gas', 'Gas'),
+        ('Internet', 'Internet'),
+        ('Luz', 'Luz'),
+        ('Telefono', 'Teléfono'),
+        ('Otro', 'Otro'),
+    )
+    tipo = models.CharField(max_length=12,choices=TIPO_CUENTA_CHOICES)
+    descripcion = models.CharField(max_length=100,blank=True,null=True)
+    monto = models.IntegerField(default=0)
+    fecha_ingreso = models.DateTimeField()
+    fecha_pago = models.DateTimeField(blank=True,null=True)
+    pagada = models.BooleanField(default=False,blank=True)
+
 
 class Compra_Inmueble(models.Model):
     item = models.ForeignKey(Item_Inmueble)
     cantidad = models.IntegerField()
     costo = models.IntegerField()
     fecha = models.DateTimeField()
-
-class Item_Ventas(models.Model):
-    tipo = models.ForeignKey(Tipo_Item)
-    descripcion = models.CharField(max_length=100)
-    valor_venta = models.IntegerField(default=0)
-
-class Tipo_Item(models.Model):
-    tipo = models.CharField(max_length=40)
-
-class Inventario_Producto(models.Model):
-    item = models.ForeignKey(Item_Ventas)
-    cantidad = models.IntegerField(default=1)
-    ultima_mod = models.DateTimeField()
-
-class Item_Inmueble(models.Model):
-    descripcion = models.Charfield(max_length=200)
-    costo = models.IntegerField(default=0)
-
-class Inventario_Inmueble(models.Model):
-    item = models.ForeignKey(Item_Inmueble)
-    cantidad = models.IntegerField(default=1)
-    ultima_mod = models.DateTimeField()
-
