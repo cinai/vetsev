5052109ee21f1405ab683a1acca240a0491d5f8a
cinai <catta.ugs@gmail.com>
2016-01-05 00:01:43 -0300

    Fix timezone, display hora en caja

--- VetSev/VetSev/settings.py
+++ VetSev/VetSev/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from django.utils import timezone
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -96,7 +97,6 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
